99072f60cad9f01eeca7deb59b737afc0df7e58b
John Ayeni <johnayeni10.aj@gmail.com>
2018-06-07 13:17:13 +0100

    chore: fix server file

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');​
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
 app = express();
-app.use(serveStatic(__dirname + '/dist'));​
-var port = process.env.PORT || 5000;
-app.listen(port, () => console.log('server started ' + port));
+app.use(serveStatic(__dirname + '/dist'));
+
+// Catch all routes and redirect to the index file
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`server started ${port}`);
