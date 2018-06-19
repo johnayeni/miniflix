ee74588dd900de3b1e56659e4bed4dc665a5e53e
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2018-06-19 13:25:35 +0100

    feat: design footer component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <app-header></app-header>
     <router-view/>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
-    'app-header': Header
+    'app-header': Header,
+    'app-footer': Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,71 @@
+<template>
+	<footer class="footer">
+		<div class="container">
+			<h1>
+				<a href="https://help.netflix.com/en/contactus">
+				<!-- can create a contact us page to include our contact info -->
+				Questions? Contact us.
+				</a>
+				</h1>
+			<div class="columns">
+				<div class="column">
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/en/node/412">FAQ</a>
+					</p>
+					<p class="navbar-item">
+						<a href="http://ir.netflix.com/">Investor Relations</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/legal/privacy">Privacy</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://fast.com/">Speed Test</a>
+					</p>
+				</div>
+				<div class="column">
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/">Help</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://jobs.netflix.com/">Jobs</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/legal/privacy#cookies">Cookies Preferences</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/legal/notices">Legal Notices</a>
+					</p>
+				</div>
+				<div class="column">
+					<p class="navbar-item">
+						<!-- profile or account link goes here -->
+						<a href="">Account</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://www.netflix.com/watch">Ways to Watch</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/en/node/2101">Corporate Information</a>
+					</p>
+					<p class="navbar-item">
+						<!-- miniflix originals link goes here -->
+						<a href="">Miniflix Originals</a>
+					</p>
+				</div>
+				<div class="column">
+					<p class="navbar-item">
+						<a href="https://media.netflix.com/">Media Center</a>
+					</p>
+					<p class="navbar-item">
+						<a href="https://help.netflix.com/legal/termsofuse">Terms of Use</a>
+					</p>
+					<p class="navbar-item">
+						<!-- our contact us page goes here -->
+						<a href="">Contact Us</a>
+					</p>
+				</div>
+		</div>
+		<h1>Miniflix</h1>
+		</div>
+	</footer>
+</template>
